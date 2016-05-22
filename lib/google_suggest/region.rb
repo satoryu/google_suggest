7d1196fc837409988622314004fb8341a3880bdc
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2016-05-22 13:10:08 +0900

    GoogleSuggest::Region.host_for returns google host url for a specified region

--- lib/google_suggest/region.rb
+++ lib/google_suggest/region.rb
@@ -1,4 +1,14 @@
 class GoogleSuggest
+  DEFAULT_GOOGLE_HOST = 'www.google.com'
+
   class Region
+    def self.host_for(region_code)
+      case region_code
+      when :ac, 'ac'
+        'www.google.ac'
+      else
+        DEFAULT_GOOGLE_HOST
+      end
+    end
   end
 end
